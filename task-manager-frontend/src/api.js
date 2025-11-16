73c414f7cea29ac931567d3593ed0c34fd0a696c
Choudhary <alka.choudhary.ext@siemens.com>
2025-11-16 23:09:10 +0530

    Removed old frontend folder from backend repo

--- task-manager-frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
-export default axios.create({
-  baseURL,
-  headers: { "Content-Type": "application/json" },
-});
